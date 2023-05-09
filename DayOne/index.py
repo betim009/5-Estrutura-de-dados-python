92dfb0feba551bd2dac40d3267472800da3b9a76
Alberto <betim009@gmail.com>
2023-05-08 21:25:22 -0300

    dir atividade 01 + main_memory

--- DayOne/index.py
+++ DayOne/index.py
@@ -1,1 +1,14 @@
-print("First Commit")
+# Exercício 1:
+# Crie um projeto que simulará a arquitetura que vimos em aula.
+# Ele deverá ter um arquivo principal para a execução do programa que
+# representará nosso Sistema Operacional e duas classes,
+# que representarão a Memória Principal e a Secundária.
+
+# Cada tipo de memória vai armazenar os dados na memória que ela representa,
+# sendo a Principal aquela que armazena tudo em memória RAM e
+# a secundária no disco. Através do Python estaremos fazendo
+# chamadas ao Sistema Operacional para realizar essas tarefas
+# para nós, pois ele melhor do que ninguém saberá utilizar as memórias.
+# O objetivo do nosso script será realizar a soma de uma lista de números
+# aleatórios utilizando as duas implementações de memória.
+# Os dados serão sempre armazenados como strings!

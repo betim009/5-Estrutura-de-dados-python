805d8d1e3eec5bc960533453e1015ebd291b2040
Alberto <betim009@gmail.com>
2023-05-09 18:09:23 -0300

    Dia 3 - Arrays

---
+++ DayThree/index.py
@@ -0,0 +1,13 @@
+"""
+Dia 03 - Arrays:
+
+Você será capaz de
+Entender o que são estruturas de dados (ED)
+
+Entender o que são tipos abstratos de dados (TAD)
+
+Reconhecer arrays e seu funcionamento em baixo nível
+
+Aplicar arrays multi-dimensionais;
+
+"""
---
+++ DayTwo/index.py
@@ -0,0 +1,3 @@
+"""
+Conteúdo por enquanto vazio.
+"""

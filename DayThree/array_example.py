b6522abb2003e02282b202e3bf7d03684c74f7ec
Alberto <betim009@gmail.com>
2023-05-10 03:16:07 -0300

    size memory

--- DayThree/array_example.py
+++ DayThree/array_example.py
@@ -1,3 +1,6 @@
+import sys
+
+
 class ListaArray:
     def __init__(self):
         self.data = []
@@ -24,12 +27,18 @@ array = ListaArray()
 array.set(0, "Alberto")
 array.set(1, "Creusa")
 array.set(2, "Carlos")
+array.set(3, "Marcela")
 
 # acessando o elemento do array pelo index
 print(array.get(0))
 print(array.get(1))
 print(array.get(2))
 print("---------- fim do print ----------")
+
+# tamanho da memoria
+array_memory_size = sys.getsizeof(array.data)
+print(f"Memory size: {array_memory_size}")
+print("--------------------")
 
 # podemos iterar sobre seus elementos da seguinte maneira
 index = 0
